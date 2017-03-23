37bf1a0447c2b961bf30380a76f41e58a7ff5b82
Hung Phan <boyhocyeuht@outlook.com>
2017-03-23 23:04:18 +0700

    Create Project By Hung Phan

---
+++ app/src/main/java/com/example/deleting/timkiemvieclam/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.deleting.timkiemvieclam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
